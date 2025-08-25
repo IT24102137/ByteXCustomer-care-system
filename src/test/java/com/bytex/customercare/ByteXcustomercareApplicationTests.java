5344756e551024eb3706188953f2955d241ab92f
Janith Deshan <IT24102137@my.sliit.lk>
2025-08-26 02:03:51 +0530

    Add files via upload

---
+++ src/test/java/com/bytex/customercare/ByteXcustomercareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytex.customercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteXcustomercareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
